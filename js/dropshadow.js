dfb6ef89a77444d928410b998d4164bfdcb58079
minint <minint.dsk@gmail.com>
2021-03-01 21:52:57 -0600

    404 completed

--- js/dropshadow.js
+++ js/dropshadow.js
@@ -1,10 +1,10 @@
 var shadowstring = '';
 var decaylength = 100;
-var decaystrength = 2000;
+var decaystrength = 1600;
 var angle = 0.0;
 
 for(var i=0; i<decaylength; i++){
-    shadowstring += angle*i +'px '+i+'px rgba(257,257,257,'+((decaylength-i)/decaystrength)+'), ';
+    shadowstring += angle*i +'px '+i+'px rgba(138,3,3,'+((decaylength-i)/decaystrength)+'), ';
 }
 shadowstring = shadowstring.substr(0, shadowstring.length-2);
 
